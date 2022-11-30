a362d76ab463805a916ce3449f22886f7aace53e
ismail BAŞARAN <basaran.ismaill@gmail.com>
2022-11-30 19:40:48 +0300

    lamba yak

---
+++ js/degiskenler.js
@@ -0,0 +1,20 @@
+var a = 5; 
+let b = "ismail";
+var g = 'i'
+let c = new Date()
+var d = true;
+var e = [1,2,3]
+var f = 3.14
+var person = {
+    name: 'ismail',
+    age:38,
+    tel: 12345678,
+    ogrencileri: [
+      {
+        name:'ahmet'
+      },
+      {
+        name: 'mehmet'
+      }
+    ]
+}
