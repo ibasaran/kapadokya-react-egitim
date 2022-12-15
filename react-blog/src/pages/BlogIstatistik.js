6d41c309c14e151e7e32a2b96f1df88ac02acd4b
ismail BAŞARAN <basaran.ismaill@gmail.com>
2022-12-15 21:58:19 +0300

    table usage

--- react-blog/src/pages/BlogIstatistik.js
+++ react-blog/src/pages/BlogIstatistik.js
@@ -1,7 +1,58 @@
+import { useState } from "react";
+
 const BlogIstatistik = () => {
 
+    const [blogs,setBlogs] = useState(
+        [
+            {
+                id:1,
+                baslik: 'Javascript kurulum',
+                detay: 'Javascript şöyle kurulur...',
+                goruntulenme:3
+            },
+            {
+                id:2,
+                baslik: 'REactjs kurulum',
+                detay: 'Javascript şöyle kurulur...',
+                goruntulenme:5
+            },
+            {
+                id:3,
+                baslik: 'Database kurulum',
+                detay: 'Javascript şöyle kurulur...',
+                goruntulenme:7
+            },
+        ]
+    )
+
+        const blogEkle = () => {
+            setBlogs([...blogs, {id:4, baslik:'Java kurulum', detay:'Java kurulum detay', goruntulenme:0}])
+        }
+
     return (
-        <h1>Blog İstatistik Sayfası</h1>
+        <>
+            <button onClick={blogEkle}>Yeni blog ekle</button>
+            <table style={{border:'1px solid'}}>
+                <thead>
+                    <th style={{border:'1px solid'}} >Blog Başlığı</th>
+                    <th style={{border:'1px solid'}}>Kaç Kez Görüntülendi</th>
+                </thead>
+                <tbody>
+                   {
+                    blogs.map( (blog,index) => {
+                        return (
+                            <tr key={index + 'tr'}>
+                                <td>
+                                    {blog.baslik}
+                                </td>
+                                <td>{blog.goruntulenme}</td>
+                            </tr>
+                        )
+                    } )
+                   }
+                </tbody>
+            </table>
+        </>
     )
 }
 
